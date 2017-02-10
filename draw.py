40199b2ba1e6e73230e00158ef53677c259d9b8c
Sandro Padin <sandropadin@gmail.com>
2017-02-10 04:45:55 -0800

    Remove create_copy option from draw_bounding_boxes

--- draw.py
+++ draw.py
@@ -1,10 +1,7 @@
 import cv2
 import numpy as np
 
-def draw_bounding_boxes(image, bounding_boxes, create_copy=False):
-    if create_copy:
-        image = np.copy(image)
-
+def draw_bounding_boxes(image, bounding_boxes):
     for (start, end) in bounding_boxes:
         cv2.rectangle(image, start, end, (0, 0, 255), 4)
 
