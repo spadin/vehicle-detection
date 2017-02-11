14b6ad920f928edba669ab45717e7d25a7786dba
Sandro Padin <sandropadin@gmail.com>
2017-02-11 06:43:31 -0800

    Update draw_bounding_boxes

--- draw.py
+++ draw.py
@@ -1,8 +1,8 @@
 import cv2
 import numpy as np
 
-def draw_bounding_boxes(image, bounding_boxes):
-    for (start, end) in bounding_boxes:
-        cv2.rectangle(image, start, end, (0, 0, 255), 4)
+def draw_bounding_boxes(image, bounding_boxes, color=(0, 0, 255)):
+    for ((start_x, start_y), (end_x, end_y)) in bounding_boxes:
+        cv2.rectangle(image, (start_x, start_y), (end_x, end_y), color, 4)
 
     return image
